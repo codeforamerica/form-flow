4764f9ff43b703e9b76551ff8ee6353dd97d5f75
Colt Borg <cborg@codeforamerica.org>
2022-10-18 17:43:14 -0500

    Contain condition/action classpath instead of strictly equal
    Co-authored-by: Alex Gonzalez <agonzalez@codeforamerica.org>

--- src/main/java/formflow/library/config/TemplateManager.java
+++ src/main/java/formflow/library/config/TemplateManager.java
@@ -15,89 +15,92 @@ import java.util.stream.Collectors;
 @Data
 public class TemplateManager {
 
-    private HashMap<String, Condition> conditions;
-    private HashMap<String, Action> actions;
+  private HashMap<String, Condition> conditions;
+  private HashMap<String, Action> actions;
 
-    public TemplateManager(String conditionsClassPath, String actionsClassPath) {
-        this.conditions = new HashMap<>();
-        this.actions = new HashMap<>();
+  public TemplateManager(String conditionsClassPath, String actionsClassPath) {
+    this.conditions = new HashMap<>();
+    this.actions = new HashMap<>();
 
-        System.out.println(String.format("In template manager and loading: \n\tConditions: %s\n\tActions: %s)",
-                conditionsClassPath, actionsClassPath));
+    System.out.printf("In template manager and loading: \n\tConditions: %s\n\tActions: %s%n",
+        conditionsClassPath, actionsClassPath);
 
+    try {
+      // get all the conditions located in conditions, load them
+      List<ClassPath.ClassInfo> classes =
+          ClassPath.from(ClassLoader.getSystemClassLoader())
+              .getAllClasses()
+              .stream()
+              .filter(clazz -> clazz.getPackageName().contains(conditionsClassPath))
+              .toList();
+
+      classes.forEach(clazz -> {
         try {
-            // get all the conditions located in conditions, load them
-            List<ClassPath.ClassInfo> classes =
-                    ClassPath.from(ClassLoader.getSystemClassLoader())
-                            .getAllClasses()
-                            .stream()
-                            .filter(clazz -> clazz.getPackageName().equalsIgnoreCase(conditionsClassPath))
-                            .collect(Collectors.toList());
-
-            classes.forEach(clazz -> {
-                try {
-                    Constructor<?> ctor = Class.forName(clazz.getName()).getConstructor();
-                    Condition condition = (Condition) ctor.newInstance();
-                    System.out.println("Loading Condition: " + clazz.getSimpleName());
-                    this.conditions.put(clazz.getSimpleName(), condition);
-                } catch (Exception e) {
-                    System.out.println(
-                            String.format("Encountered %s exception when creating %s Condition: %s",
-                                    e.getClass(), clazz.getName(), e.getMessage())
-                    );
-                }
-            });
-        } catch (IOException e) {
-            System.out.println("Caught IOException while loading Conditions: " + e.getMessage());
+          Constructor<?> ctor = Class.forName(clazz.getName()).getConstructor();
+          Condition condition = (Condition) ctor.newInstance();
+          System.out.println("Loading Condition: " + clazz.getSimpleName());
+          this.conditions.put(clazz.getSimpleName(), condition);
+        } catch (Exception e) {
+          System.out.printf("Encountered %s exception when creating %s Condition: %s%n",
+              e.getClass(), clazz.getName(), e.getMessage());
         }
+      });
 
+      if (classes.size() == 0) {
+        System.out.print("No conditions classes \uD83D\uDE1E%n");
+      }
+    } catch (IOException e) {
+      System.out.println("Caught IOException while loading Conditions: " + e.getMessage());
+    }
+
+    try {
+      // get all the actions located in the actions class path, load them
+      List<ClassPath.ClassInfo> classes =
+          ClassPath.from(ClassLoader.getSystemClassLoader())
+              .getAllClasses()
+              .stream()
+              .filter(clazz -> clazz.getPackageName().contains(actionsClassPath)).toList();
+
+      classes.forEach(clazz -> {
         try {
-            // get all the actions located in the actions class path, load them
-            List<ClassPath.ClassInfo> classes =
-                    ClassPath.from(ClassLoader.getSystemClassLoader())
-                            .getAllClasses()
-                            .stream()
-                            .filter(clazz -> clazz.getPackageName().equalsIgnoreCase(actionsClassPath))
-                            .collect(Collectors.toList());
-
-            classes.forEach(clazz -> {
-                try {
-                    Constructor<?> ctor = Class.forName(clazz.getName()).getConstructor();
-                    Action action = (Action) ctor.newInstance();
-                    System.out.println("Loading Action: " + clazz.getSimpleName());
-                    this.actions.put(clazz.getSimpleName(), action);
-                } catch (Exception e) {
-                    System.out.println(
-                            String.format("Encountered %s exception when creating %s Action: %s",
-                                    e.getClass(), clazz.getName(), e.getMessage())
-                    );
-                }
-            });
-        } catch (IOException e) {
-            System.out.println("Caught IOException while loading Action: " + e.getMessage());
+          Constructor<?> ctor = Class.forName(clazz.getName()).getConstructor();
+          Action action = (Action) ctor.newInstance();
+          System.out.println("Loading Action: " + clazz.getSimpleName());
+          this.actions.put(clazz.getSimpleName(), action);
+        } catch (Exception e) {
+          System.out.printf("Encountered %s exception when creating %s Action: %s%n",
+              e.getClass(), clazz.getName(), e.getMessage());
         }
-    }
+      });
 
-    public Boolean runCondition(String conditionName, Submission submission) {
-        System.out.println("Running condition " + conditionName);
-        Condition condition = conditions.get(conditionName);
-        if (condition == null) {
-            System.out.println("Condition not found: " + conditionName);
-            // TODO throw error?
-            return null;
-        }
-        return condition.run(submission);
+      if (classes.size() == 0) {
+        System.out.print("No action classes \uD83D\uDE1E%n");
+      }
+    } catch (IOException e) {
+      System.out.println("Caught IOException while loading Action: " + e.getMessage());
     }
+  }
 
-    public Boolean runCondition(String conditionName, Submission submission, String uuid) {
-        System.out.println("Running condition " + conditionName + " data: " + uuid);
-         Condition condition = conditions.get(conditionName);
-        if (condition == null) {
-            System.out.println("Condition not found: " + conditionName);
-            // TODO throw error?
-            return null;
-        }
-        return condition.run(submission, uuid);
+  public Boolean runCondition(String conditionName, Submission submission) {
+    System.out.println("Running condition " + conditionName);
+    Condition condition = conditions.get(conditionName);
+    if (condition == null) {
+      System.out.println("Condition not found: " + conditionName);
+      // TODO throw error?
+      return null;
     }
+    return condition.run(submission);
+  }
+
+  public Boolean runCondition(String conditionName, Submission submission, String uuid) {
+    System.out.println("Running condition " + conditionName + " data: " + uuid);
+    Condition condition = conditions.get(conditionName);
+    if (condition == null) {
+      System.out.println("Condition not found: " + conditionName);
+      // TODO throw error?
+      return null;
+    }
+    return condition.run(submission, uuid);
+  }
 
 }
