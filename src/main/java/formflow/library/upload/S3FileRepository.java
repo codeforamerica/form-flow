34895e6518aace3da2f3c312e61cdd43e1189ea2
Alex Gonzalez <spokenbird@gmail.com>
2022-10-24 11:52:43 -0700

    WIP Adding AWS dependencies
    Co-authored-by: Ben Calegari <bcalegari@codeforamerica.org>
    Co-authored-by: Chibuisi Enyia <cenyia@codeforamerica.org>

--- src/main/java/formflow/library/upload/S3FileRepository.java
+++ src/main/java/formflow/library/upload/S3FileRepository.java
@@ -1,13 +1,5 @@
 package formflow.library.upload;
 
-import com.amazonaws.SdkClientException;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.DeleteObjectRequest;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
-import com.amazonaws.services.s3.transfer.TransferManager;
-import com.amazonaws.util.IOUtils;
 import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,16 +8,16 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 @Slf4j
 public class S3FileRepository implements FileRepository {
-
-  private final TransferManager transferManager;
+  private final AWSCredentials credentials = new BasicAWSCredentials(π)
   private final String bucketName;
-
   private final AmazonS3 s3Client;
 
-  public S3DocumentRepository(
-      TransferManager transferManager,
-      @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection") AmazonS3 s3Client) {
-    this.s3Client = s3Client;
+  public S3DocumentRepository() {
+    this.s3Client = AmazonS3ClientBuilder
+        .standard()
+        .withCredentials(new AWSStaticCredentialsProvider(credentials))
+        .withRegion(Regions.US_EAST_2)
+        .build();
     this.bucketName = System.getenv("S3_BUCKET");
     this.transferManager = transferManager;
   }
