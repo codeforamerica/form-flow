007c7437f975495e743880bb8d79a3f94d85f41d
spokenbird <agonzalez@codeforamerica.org>
2022-11-18 11:32:48 -0800

    Update naming convention for user files
    Co-authored-by: Bethany Seeger <bseeger@codeforamerica.org>
    Co-authored-by: Ben Calegari <bcalegari@codeforamerica.org>
    Co-authored-by: Cypress Borg <cborg@codeforamerica.org>
    Co-authored-by: Chibuisi Enyia <cenyia@codeforamerica.org>

--- src/main/java/formflow/library/UploadController.java
+++ src/main/java/formflow/library/UploadController.java
@@ -6,8 +6,7 @@ import formflow.library.data.SubmissionRepositoryService;
 import formflow.library.data.UploadedFileRepositoryService;
 import formflow.library.data.UserFile;
 import formflow.library.upload.CloudFileRepository;
-import java.util.ArrayList;
-import java.util.Collections;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -67,9 +66,10 @@ public class UploadController extends FormFlowController {
       }
       String dropZoneInstanceName = formData.get("inputName");
       String fileExtension = Files.getFileExtension(Objects.requireNonNull(file.getOriginalFilename()));
-      String uploadLocation = String.format("%s/%s-%s.%s", submission.getId(), dropZoneInstanceName, userFileId,
+      String uploadLocation = String.format("%s/%s_%s_%s.%s", submission.getId(), flow, dropZoneInstanceName, userFileId,
           fileExtension);
-      String thumbLocation = String.format("%s/%s-%s-thumbnail.txt", submission.getId(), dropZoneInstanceName, userFileId);
+      String thumbLocation = String.format("%s/%s_%s_%s-thumbnail.txt", submission.getId(), flow, dropZoneInstanceName,
+          userFileId);
 
       cloudFileRepository.upload(uploadLocation, file);
       if (file.getContentType() != null && UserFile.isSupportedImage(file.getContentType())) {
@@ -85,65 +85,6 @@ public class UploadController extends FormFlowController {
 
       Long newFileId = uploadedFileRepositoryService.save(uploadedFile);
 
-      // 1. is in a subflow
-      //   A.  Does inputName already exist in submission?
-      //   B. inputName doesn't exist, put file
-      // 2. not in a subflow
-      //   A. inputName doesn't exist, put file
-      //   B. inputName doesn't exist in subflow, put file
-/*
-      Map<String, Object> inputData = submission.getInputData();
-
-      if (subflow != null) {
-        log.info("has subflow: " + subflow);
-        if (inputData.containsKey(subflow)) {
-          log.info("here");
-          ArrayList<Object> subflowData = (ArrayList<Object>) inputData.get(subflow);
-          if (subflowData.containsKey(dropZoneInstanceName)) {
-            log.info("has existing data for file widget");
-
-            ArrayList<Long> fileIds = (ArrayList<Long>) ((HashMap<String, Object>) subflowData.get(dropZoneInstanceName)).get(
-                USER_FILE_ID_KEY);
-            fileIds.add(uploadedFile.getFile_id());
-          } else {
-            log.info("does not have existing data for widget");
-            HashMap<String, ArrayList<Long>> userFiles = new HashMap<>();
-            userFiles.put(USER_FILE_ID_KEY, new ArrayList<>(Collections.singletonList(uploadedFile.getFile_id())));
-            subflowData.put(dropZoneInstanceName, userFiles);
-          }
-        } else {
-          log.info("Does not have existing data for file widget");
-          // add subflow... in the case of the first screen of a subflow
-          HashMap<String, ArrayList<Long>> userFiles = new HashMap<>();
-          userFiles.put(USER_FILE_ID_KEY, new ArrayList<>(Collections.singletonList(uploadedFile.getFile_id())));
-
-          HashMap<String, Object> newSubflowWithFiles = new HashMap<>();
-          newSubflowWithFiles.put(dropZoneInstanceName, userFiles);
-          inputData.put(subflow, newSubflowWithFiles);
-        }
-      } else {
-        log.info("NOT in a subflow");
-        // are there files already associated with this submission?
-        if (submission.getInputData().containsKey(dropZoneInstanceName)) {
-          log.info("has existing data for file widget");
-          // yes, there are already files, add this one on
-          HashMap<String, ArrayList<Long>> userFiles = (HashMap<String, ArrayList<Long>>) submission.getInputData()
-              .get(dropZoneInstanceName);
-          userFiles.get(USER_FILE_ID_KEY).add(uploadedFile.getFile_id());
-        } else {
-          log.info("Does not have existing data for file widget");
-          // no, there are no files, create the array and add this as the first one
-          HashMap<String, ArrayList<Long>> userFiles = new HashMap<>();
-          userFiles.put(USER_FILE_ID_KEY, new ArrayList<>(Collections.singletonList(uploadedFile.getFile_id())));
-          submission.getInputData().put(dropZoneInstanceName, userFiles);
-        }
-      }
-
-      submissionRepositoryService.save(submission);
-*/
-      // Once we merge code: we will need a unique identifier for the dropzone input widget to associated this with in the JSON
-      // TODO: pass back new file id in response body
-      //
       log.info("Created new file with id: " + newFileId);
       return ResponseEntity.status(HttpStatus.OK).body(newFileId);
     } catch (Exception e) {
