11de87209023558da9071e8f675ec9907a09926b
Bethany Seeger <bseeger@codeforamerica.org>
2023-01-13 09:45:42 -0500

    Tries to be more generic

--- src/main/resources/db/migration/V4__user_files_change_primary_key_type.sql
+++ src/main/resources/db/migration/V4__user_files_change_primary_key_type.sql
@@ -1,6 +1,6 @@
 -- This will make the original file_id a column a uuid column
-create extension if not exists "uuid-ossp";
-alter table user_files add column new_id uuid DEFAULT uuid_generate_v4();
+
+alter table user_files add column new_id uuid DEFAULT gen_random_uuid();
 alter table user_files drop column file_id;
 alter table user_files rename column new_id to file_id;
 alter table user_files alter column file_id set not null;
