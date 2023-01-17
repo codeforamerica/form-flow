f24a1c07c4dd08e23ce4bddf9e63ad1265d00a2f
bencalegari <bcalegari@codeforamerica.org>
2023-01-17 15:22:27 -0800

    wip: might be time to fold em
    Co-authored-by: Alex Gonzalez <agonzalez@codeforamerica.org>

--- src/main/resources/META-INF/resources/webjars/form-flow/0.0.1/js/qunit-upload-tests.js
+++ src/main/resources/META-INF/resources/webjars/form-flow/0.0.1/js/qunit-upload-tests.js
@@ -1,58 +1,26 @@
 
 QUnit.module('Dropzone', function () {
-  const testFile = new File([""], "test.pdf", {type: "application/pdf"});
-
   QUnit.testStart(function () {
     window['myDropZoneuploadTest'].removeAllFiles();
   });
 
   QUnit.test('loads accepted files from application.yaml', assert => {
-    assert.equal(window['myDropZoneuploadTest'].options.acceptedFiles,
-        ".jpeg,.jpg,.png,.pdf,.bmp,.gif,.doc,.docx,.odt,.ods,.odp");
+    assert.equal(window['myDropZoneuploadTest'].options.acceptedFiles, ".jpeg, .fake");
   });
 
-  QUnit.test('deleting an upload file removes it from the page', assert => {
-    const uploadComplete = assert.async();
-    const deleteComplete = assert.async();
-
-    window['myDropZoneuploadTest'].on('success', () => {
-      assert.equal(document.querySelector('.dz-remove').innerHTML, 'delete');
-      document.querySelector('.dz-remove').click();
-      window.confirm = () => true;
-      uploadComplete();
-    });
-
-    window['myDropZoneuploadTest'].on('removedFile', () => {
-      assert.equal(document.querySelectorAll('.dz-preview').length, 0);
-      assert.equal(window['myDropZoneuploadTest'].files.length, 0);
-
-      deleteComplete();
-    });
-
-
-    window['myDropZoneuploadTest'].addFile(testFile);
+  QUnit.test('loads max file size from application.yaml', assert => {
+    assert.equal(window['myDropZoneuploadTest'].options.maxFilesize, "17");
   });
 
-  // QUnit.test('cancelling a file not fully uploaded removes it from the page', assert => {
-  //   window['myDropZoneuploadTest'].addFile(testFile);
-  //   assert.equal(document.querySelector('.dz-remove').innerHTML, 'cancel');
-  //   document.querySelector('.dz-remove').click();
-  //   assert.equal(document.querySelectorAll('.dz-preview').length, 0);
-  //   assert.equal(window['myDropZoneuploadTest'].files.length, 0);
-  //
-  //   // assert.equal(document.querySelectorAll('.dz-preview').length, 1);
-  //   // assert.equal(document.querySelector('.dz-remove').innerHTML, 'delete');
-  //   // window.confirm = () => true;
-  //   //
-  //   // assert.equal(document.querySelectorAll('.dz-preview').length, 0);
-  //   // assert.equal(window['myDropZoneuploadTest'].files.length, 0);
-  // });
+  QUnit.test('loads max number of files from application.yaml', assert => {
+    assert.equal(window['myDropZoneuploadTest'].options.maxFiles, "3");
+  });
 
-
-  // QUnit.test('clicking remove removes testFile from the page', assert => {
-  // });
+  QUnit.test('loads thumbnail height/width from application.yaml', assert => {
+    assert.equal(window['myDropZoneuploadTest'].options.thumbnailHeight, "50");
+    assert.equal(window['myDropZoneuploadTest'].options.thumbnailWidth, "100");
+  });
 });
-
 QUnit.testDone(function () {
   document.querySelector("#uploadTest").className += " display-none";
 });
--- src/test/java/formflow/library/upload/UploadJourneyTest.java
+++ src/test/java/formflow/library/upload/UploadJourneyTest.java
@@ -1,12 +1,14 @@
 package formflow.library.upload;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.awaitility.Awaitility.await;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 import formflow.library.utilities.AbstractBasePageTest;
 import java.io.IOException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {"form-flow.path=flows-config/test-upload.yaml"}, webEnvironment = RANDOM_PORT)
@@ -22,6 +24,9 @@ public class UploadJourneyTest extends AbstractBasePageTest {
   @Test
   void runQunitTests() {
     takeSnapShot("src/test/resources/qunit-results.png");
+    await().until(
+        () -> driver.findElements(By.id("qunit-testresult-display")).get(0).getAttribute("innerHTML")
+            .contains("tests completed in"));
     assertThat(testPage.getElementText("qunit-testresult-display")).doesNotContain("global failure");
     assertThat(testPage.getElementText("qunit-testresult-display")).contains("0 failed");
   }
