0e55a61a8a0b44a47b59e872349bdfd7ea00e9ac
Lauren Kemperman <117678574+lkemperman-cfa@users.noreply.github.com>
2023-02-08 13:45:37 -0500

    fix money validator - previously disallowed single digit numbers (#131)

--- src/main/java/formflow/library/data/validators/MoneyValidator.java
+++ src/main/java/formflow/library/data/validators/MoneyValidator.java
@@ -9,6 +9,6 @@ public class MoneyValidator implements ConstraintValidator<Money, String> {
   @Override
   public boolean isValid(String value, ConstraintValidatorContext context) {
     // Matches one or more digits, a dot and 2 digits after the dot.
-    return Pattern.matches("(^(0|[1-9][0-9]*)\\.?\\d{1,2}$)?", value);
+    return Pattern.matches("(^(0|([1-9]\\d*))?(\\.\\d{1,2})?$)?", value);
   }
 }
