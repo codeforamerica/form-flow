c56d682403f11ace00df5378321e083d641e80fe
Lauren Kemperman <117678574+lkemperman-cfa@users.noreply.github.com>
2023-02-14 11:53:20 -0500

    add additional js function to formFlowDropZone.js to disable if no files. could be consolidated into same function if we also pass the class (#137)

--- src/main/resources/META-INF/resources/webjars/form-flow/0.0.1/js/formFlowDropZone.js
+++ src/main/resources/META-INF/resources/webjars/form-flow/0.0.1/js/formFlowDropZone.js
@@ -13,6 +13,21 @@ FormFlowDZ = {
     if (window[dropzonePrefix + inputName].files.length > 0) {
       $(document.getElementById(idToHide)).removeClass("display-none");
     }
+  },
+    disableIfNoFiles: function (inputName, idToDisable) {
+      window[dropzonePrefix + inputName].on('success', function () {
+        $(document.getElementById(idToDisable)).removeClass("disabled");
+      });
+
+      window[dropzonePrefix + inputName].on('removedfile', function () {
+        if (window[dropzonePrefix + inputName].files.length === 0) {
+          $(document.getElementById(idToDisable)).addClass("disabled");
+        }
+      });
+
+      if (window[dropzonePrefix + inputName].files.length > 0) {
+        $(document.getElementById(idToDisable)).removeClass("disabled");
+      }
   }
 }
 
