6db63fd4f76ef82b37f8731283e7633f3fce5e50
Lauren Kemperman <lkemperman@codeforamerica.org>
2023-03-08 12:21:48 -0500

    add the failure example

--- src/test/java/formflow/library/framework/CrossValidationTest.java
+++ src/test/java/formflow/library/framework/CrossValidationTest.java
@@ -1,6 +1,5 @@
 package formflow.library.framework;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -8,12 +7,10 @@ import formflow.library.ScreenController;
 import formflow.library.data.Submission;
 import formflow.library.data.SubmissionRepositoryService;
 import formflow.library.utilities.AbstractMockMvcTest;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -53,25 +50,22 @@ public class CrossValidationTest extends AbstractMockMvcTest {
                         "contactMethod", List.of("emailPreferred")));
     }
 
-//    @Test
-//    void shouldSaveTotalIncome() throws Exception {
-//        String subflowUuid = UUID.randomUUID().toString();
-//
-//        List<Map<String, Object>> subflowList = new ArrayList<>();
-//
-//        subflowList.add(Map.of("uuid", subflowUuid));
-//        subflowList.add(Map.of("uuid", UUID.randomUUID().toString(),
-//                "textInput", "2200",
-//                "iterationIsComplete", true));
-//        subflowList.add(Map.of("uuid", UUID.randomUUID().toString(),
-//                "textInput", "3330",
-//                "iterationIsComplete", true));
-//
-//        submission.getInputData().put("income", subflowList);
-//
-//        postToUrlExpectingSuccess("/testFlow/next", "/testFlow/subflowReview",
-//                Map.of("textInput", List.of("1000")), subflowUuid);
-//
-//        assertThat(submission.getInputData().get("totalIncome")).isEqualTo(6530.0);
-//    }
+    @Test
+    void shouldAcceptPhoneNumberWithPreference() throws Exception {
+        postExpectingSuccess("contactInfoPreference",
+                Map.of(
+                        "phoneNumber", List.of("123-456-7891"),
+                        "contactMethod", List.of("phonePreferred")));
+    }
+
+    @Test
+    void shouldFailWithPhoneNumberPreferenceNoPhone() throws Exception {
+        final String phoneErrorMessage = "please provide a phone number";
+        postExpectingFailure("contactInfoPreference",
+                Map.of(
+                        "contactMethod", List.of("phonePreferred"),
+                        "phoneNumber", List.of("")));
+
+        assertPageHasInputError("contactInfoPreference", "phoneNumber", phoneErrorMessage);
+    }
 }
