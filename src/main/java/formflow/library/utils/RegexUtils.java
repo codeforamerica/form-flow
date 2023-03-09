0888cfc704fec05e4aaa3a4c282864d4f3e8f95a
chibuisienyia <cenyia@codeforamerica.org>
2023-03-09 16:46:40 -0600

    Move RegexUtils to lib #184432411
    Co-authored-by: Alex Gonzalez <agonzalez@codeforamerica.org>

---
+++ src/main/java/formflow/library/utils/RegexUtils.java
@@ -0,0 +1,6 @@
+package formflow.library.utils;
+
+public class RegexUtils {
+
+  public static final String EMAIL_REGEX = "[a-zA-Z0-9!#$%&'*+=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&'*+=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?";
+}
