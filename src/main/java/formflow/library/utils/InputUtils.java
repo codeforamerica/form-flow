ef3abbcfc9519fa024bdaea54f6469e3734ce44a
spokenbird <agonzalez@codeforamerica.org>
2023-05-05 11:24:44 -0700

    Update input templates to use InputUtils.getFieldData
    [#183082612]
    
    Co-authored-by: Bethany Seeger <bseeger@codeforamerica.org>

--- src/main/java/formflow/library/utils/InputUtils.java
+++ src/main/java/formflow/library/utils/InputUtils.java
@@ -1,11 +1,14 @@
 package formflow.library.utils;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
 import java.util.Map;
 
 /**
  * Utility class to help with parsing input from forms
  */
+@Slf4j
 public class InputUtils {
 
     /**
@@ -35,12 +38,22 @@ public class InputUtils {
         return false;
     }
 
+    /**
+     * Static function that returns a Map of input data for use in the thymeleaf template. If you are in a subflow,
+     * the Map will be set to currentSubflowItem and if you are not in a subflow, the Map will be set to
+     * inputData (submission inputData).
+     *
+     * @param currentSubflowItem a Map of input field name to value specific to the current subflow iteration, can be null.
+     * @param inputData          a Map of all input field names to values held in the submission object, can but probably shouldn't be null.
+     * @return currentSubflowItem if it is not null, otherwise inputData.
+     */
     public static Map<String, Object> getFieldData(Map<String, Object> currentSubflowItem, Map<String, Object> inputData) {
         if (currentSubflowItem != null) {
             return currentSubflowItem;
         } else if (inputData != null) {
             return inputData;
         }
+        log.warn("No data could be found for use in templates.");
         return Map.of();
     }
 }
