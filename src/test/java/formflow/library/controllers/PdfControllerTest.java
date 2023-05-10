00c09559e889ee6d1777db50c63308d4cfdcc554
spokenbird <agonzalez@codeforamerica.org>
2023-05-10 16:09:06 -0700

    Secure PdfController download endpoint
    - User can only download a PDF associated with their currently active sessionId
    
    [#185152956]

--- src/test/java/formflow/library/controllers/PdfControllerTest.java
+++ src/test/java/formflow/library/controllers/PdfControllerTest.java
@@ -33,7 +33,7 @@ public class PdfControllerTest extends AbstractMockMvcTest {
     @BeforeEach
     public void setUp() throws Exception {
         flow = "ubi";
-        PdfController pdfController = new PdfController(pdfGenerator);
+        PdfController pdfController = new PdfController(messageSource, pdfGenerator);
         mockMvc = MockMvcBuilders.standaloneSetup(pdfController).build();
         submission = Submission.builder().id(UUID.randomUUID()).build();
         filledPdf = mock(PdfFile.class);
@@ -53,5 +53,18 @@ public class PdfControllerTest extends AbstractMockMvcTest {
 
         verify(pdfGenerator, times(1)).generate(flow, submission.getId());
         verify(filledPdf, times(1)).finalizeForSending();
+    }
+
+    @Test
+    public void shouldNotAllowDownloadingAPdfWithADifferentSubmissionIdThanTheActiveSession() throws Exception {
+        session.setAttribute("id", UUID.randomUUID());
+
+        mockMvc.perform(get("/download/ubi/" + submission.getId()).session(session))
+                .andExpect(status().is4xxClientError());
+
+        session.setAttribute("id", submission.getId());
+
+        mockMvc.perform(get("/download/ubi/" + submission.getId()).session(session))
+                .andExpect(status().is2xxSuccessful());
     }
 }
