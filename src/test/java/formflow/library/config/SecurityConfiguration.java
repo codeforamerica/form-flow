69a5383d16254b4921427c4bb08b039ca657ac18
Alex Gonzalez <spokenbird@gmail.com>
2023-05-11 11:14:36 -0700

    Add X-forwarded-for headers filter (#207)
    [#184553834]

--- src/test/java/formflow/library/config/SecurityConfiguration.java
+++ src/test/java/formflow/library/config/SecurityConfiguration.java
@@ -6,14 +6,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-  @Bean
-  SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity.formLogin().disable();
-    return httpSecurity.build();
-  }
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.formLogin().disable();
+        return httpSecurity.build();
+    }
+
+    @Bean
+    // Use X-Forwarded-For / X-Forwarded-Proto headers when generating full link URLs.
+    public ForwardedHeaderFilter forwardedHeaderFilter() {
+        return new ForwardedHeaderFilter();
+    }
 }
