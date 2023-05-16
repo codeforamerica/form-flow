7fd8a1656eac8da5588c7daf8d5f8baf11c9f6a2
Cypress Borg <9101728+coltborg@users.noreply.github.com>
2023-05-16 12:34:06 -0500

    Call email service 184899355 (#204)
    * Add mailgun dependency
    * Add email clients from Shiba
    * Finalize sendEmail methods
    * Add tests to MailgunEmailClient
    * Add javadoc
    * Add setSenderEmail
    
    Co-authored-by: Bethany Seeger <bseeger@codeforamerica.org>

---
+++ src/main/java/formflow/library/email/EmailClient.java
@@ -0,0 +1,56 @@
+package formflow.library.email;
+
+import java.io.File;
+import java.util.List;
+
+public interface EmailClient {
+
+    /**
+     * This sends an email with the least amount of information needed to be provided.
+     *
+     * @param subject        The subject line of the email
+     * @param recipientEmail The email address that will get the email, aka the To field
+     * @param emailBody      The plain text version of the email body
+     */
+    void sendEmail(
+            String subject,
+            String recipientEmail,
+            String emailBody
+    );
+
+    /**
+     * This sends an email with the least amount of information needed to be provided, but with attachments.
+     *
+     * @param subject        The subject line of the email
+     * @param recipientEmail The email address that will get the email, aka the To field
+     * @param emailBody      The plain text version of the email body
+     * @param attachments    A list of files that will be added as attachments to the email
+     */
+    void sendEmail(
+            String subject,
+            String recipientEmail,
+            String emailBody,
+            List<File> attachments
+    );
+
+    /**
+     * This sends an email with the most customizations.
+     *
+     * @param subject        The subject line of the email
+     * @param recipientEmail The email address that will get the email, aka the To field
+     * @param emailsToCC     A list of emails to be added into the CC field
+     * @param emailsToBCC    A list of emails to be added into the BCC field
+     * @param emailBody      The plain text version of the email body
+     * @param attachments    A list of files that will be added as attachments to the email
+     * @param requireTls     A way to make TLS required
+     */
+    void sendEmail(
+            String subject,
+            String recipientEmail,
+            List<String> emailsToCC,
+            List<String> emailsToBCC,
+            String emailBody,
+            List<File> attachments,
+            boolean requireTls
+    );
+}
