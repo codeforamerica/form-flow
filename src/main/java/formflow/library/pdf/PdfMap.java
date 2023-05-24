d8c911532c6c945b0db2199a479fb1811e19e9c9
Bethany Seeger <bseeger@codeforamerica.org>
2023-05-24 11:32:41 -0400

    Fixes and refactors the code that gets subflow fields

--- src/main/java/formflow/library/pdf/PdfMap.java
+++ src/main/java/formflow/library/pdf/PdfMap.java
@@ -14,61 +14,21 @@ import java.util.stream.Collectors;
 @NoArgsConstructor
 public class PdfMap {
 
-    String flow;
-    String pdf;
-    Map<String, Object> inputFields;
-    Map<String, Object> dbFields;
-    Map<String, PdfMapSubflow> subflowInfo;
+  String flow;
+  String pdf;
+  Map<String, Object> inputFields;
+  Map<String, Object> dbFields;
+  Map<String, PdfMapSubflow> subflowInfo;
 
-    public Map<String, Object> getAllSubflowFields() {
-        Map<String, Object> subflowFields = new HashMap<>();
+  public Map<String, Object> getAllSubflowFields() {
+    Map<String, Object> subflowFields = new HashMap<>();
 
-        AtomicInteger atomicInteger = new AtomicInteger(0);
+    subflowInfo.forEach((subflowName, subflow) -> {
+      subflowFields.putAll(subflow.getFieldsForIterations());
+    });
 
-        // TODO -- check this logic, it's probably not complete
+    return subflowFields;
+  }
 
-        subflowInfo.forEach((subflowName, subflow) -> {
-            Map<String, Object> fields = subflow.getFields();
-            String suffix = "_" + (atomicInteger.get() + 1);
 
-            for (int index = 0; index < subflow.getTotalIterations(); index++) {
-                Map<String, Object> singleIteration = new HashMap<>();
-
-                fields.forEach((key, value) -> {
-                    // either return a List<Map<String, Object>> or
-                    // return a Map<String, Object>
-                    String newKey = addSuffix(key, suffix);
-
-                    if (value instanceof Map) {
-                        //Map<String, Object> val = (Map<String, Object>) fieldEntry.getValue();
-                        // run through all elements in the list it contains
-                        Map<String, Object> values = ((Map<String, Object>) value).entrySet().stream()
-                                .map(listEntry -> {
-                                    // don't change the key name here, it's not necessary
-                                    return (Map.entry(listEntry.getKey(), listEntry.getValue() + suffix));
-                                }).collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()));
-
-                        //return (Map.entry(newKey, values));
-                        singleIteration.put(newKey, values);
-                    } else {
-                        //return (new AbstractMap.SimpleEntry(entrySet.getKey() + suffix, entrySet.getValue()));
-                        //return (Map.entry(newKey, fieldEntry.getValue() + suffix));
-                        singleIteration.put(newKey, value + suffix);
-                    }
-                });
-                subflowFields.putAll(singleIteration);
-                atomicInteger.getAndIncrement();
-            }
-        });
-        return subflowFields;
-    }
-
-    private String addSuffix(String origString, String suffix) {
-        int index = origString.lastIndexOf("[]");
-        if (index != -1) {
-            return origString.replaceFirst("\\[\\]$", suffix + "[]");
-        } else {
-            return origString + suffix;
-        }
-    }
 }
--- src/main/java/formflow/library/pdf/PdfMapSubflow.java
+++ src/main/java/formflow/library/pdf/PdfMapSubflow.java
@@ -1,7 +1,10 @@
 package formflow.library.pdf;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,4 +18,40 @@ public class PdfMapSubflow {
   int totalIterations;
   String dataAction;
   Map<String, Object> fields;
+
+  public Map<String, Object> getFieldsForIterations() {
+    Map<String, Object> iterationFields = new HashMap<>();
+    AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    for (int index = 0; index < totalIterations; index++) {
+      String suffix = "_" + (atomicInteger.get() + 1);
+
+      fields.forEach((key, value) -> {
+        String newKey = addSuffix(key, suffix);
+
+        if (value instanceof Map) {
+          Map<String, Object> values = ((Map<String, Object>) value).entrySet().stream()
+              .map(listEntry -> {
+                // don't change the key name here, it's not necessary
+                return (Map.entry(listEntry.getKey(), listEntry.getValue() + suffix));
+              }).collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()));
+
+          iterationFields.put(newKey, values);
+        } else {
+          iterationFields.put(newKey, value + suffix);
+        }
+      });
+      atomicInteger.getAndIncrement();
+    }
+    return iterationFields;
+  }
+
+  private String addSuffix(String origString, String suffix) {
+    int index = origString.lastIndexOf("[]");
+    if (index != -1) {
+      return origString.replaceFirst("\\[\\]$", suffix + "[]");
+    } else {
+      return origString + suffix;
+    }
+  }
 }
