90b87ed44b6aca170efa3bed435f314a76215d20
spokenbird <agonzalez@codeforamerica.org>
2023-05-25 16:18:06 -0700

    Add javadoc for RemoveApplicantIterationAction

--- src/test/java/formflow/library/pdf/RemoveApplicantIterationAction.java
+++ src/test/java/formflow/library/pdf/RemoveApplicantIterationAction.java
@@ -8,6 +8,14 @@ import java.util.stream.Collectors;
 
 public class RemoveApplicantIterationAction implements Action {
 
+  /**
+   * A test action that removes the applicant iteration from the subflow.
+   *
+   * @param submission The submission object.
+   * @param subflowMap The subflow map object.
+   * @return If the testSubflow contains an iteration with the firstName "Applicant", it will be removed from the subflow list and
+   * the remaining iterations will be returned.
+   */
   public List<Map<String, Object>> runSubflowAction(Submission submission, PdfMapSubflow subflowMap) {
     List<Map<String, Object>> testSubflow = (List<Map<String, Object>>) submission.getInputData().get("testSubflow");
     return testSubflow.stream().filter(iteration -> !iteration.get("firstName").equals("Applicant")).collect(Collectors.toList());
