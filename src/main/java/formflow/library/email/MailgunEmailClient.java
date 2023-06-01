6e77182ac4414b9afcda6b37a479859f009bb42d
chibuisienyia <cenyia@codeforamerica.org>
2023-06-01 04:09:48 -0500

    Updated and/or created Javadocs for EmailUtils and MailgunEmailClient
    #184899368

--- src/main/java/formflow/library/email/MailgunEmailClient.java
+++ src/main/java/formflow/library/email/MailgunEmailClient.java
@@ -36,7 +36,7 @@ public class MailgunEmailClient implements EmailClient {
    *
    * @param subject        The subject line of the email
    * @param recipientEmail The email address that will get the email, aka the To field
-   * @param emailBody      The plain text version of the email body
+   * @param emailBody      The html version of the email body
    */
   @Override
   public void sendEmail(
@@ -59,7 +59,7 @@ public class MailgunEmailClient implements EmailClient {
    *
    * @param subject        The subject line of the email
    * @param recipientEmail The email address that will get the email, aka the To field
-   * @param emailBody      The plain text version of the email body
+   * @param emailBody      The html version of the email body
    * @param attachments    A list of files that will be added as attachments to the email
    */
   @Override
@@ -85,7 +85,7 @@ public class MailgunEmailClient implements EmailClient {
    * @param recipientEmail The email address that will get the email, aka the To field
    * @param emailsToCC     A list of emails to be added into the CC field
    * @param emailsToBCC    A list of emails to be added into the BCC field
-   * @param emailBody      The plain text version of the email body
+   * @param emailBody      The html version of the email body
    * @param attachments    A list of files that will be added as attachments to the email
    * @param requireTls     A way to make TLS required
    */
