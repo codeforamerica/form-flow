b2b460828bb54431696bc9017b4caa5a84f97902
Alex Gonzalez <spokenbird@gmail.com>
2023-06-13 14:02:45 -0700

    Change VALIDATE_ADDRESS prefix to 'validate_' (#250)
    * Change VALIDATE_ADDRESS prefix to 'validate_'
    
    [#184814556]
    
    Co-authored-by: Lauren Kemperman <lkemperman@codeforamerica.org>

--- src/main/java/formflow/library/inputs/UnvalidatedField.java
+++ src/main/java/formflow/library/inputs/UnvalidatedField.java
@@ -3,6 +3,6 @@ package formflow.library.inputs;
 public enum UnvalidatedField {
   ;
   public static final String CSRF = "_csrf";
-  public static final String VALIDATE_ADDRESS = "_validate";
+  public static final String VALIDATE_ADDRESS = "validate_";
   public static final String VALIDATED = "_validated";
 }
