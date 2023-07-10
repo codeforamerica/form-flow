6fab871e1827ebbfcfcc821c97ed698c67cb5718
Ben Calegari <bcalegari@codeforamerica.org>
2023-07-10 14:30:32 -0700

    Use non-deprecated security method (#309)

--- src/test/java/formflow/library/config/SecurityConfiguration.java
+++ src/test/java/formflow/library/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
@@ -14,7 +15,7 @@ public class SecurityConfiguration {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.formLogin().disable();
+        httpSecurity.formLogin(AbstractHttpConfigurer::disable);
         return httpSecurity.build();
     }
 
