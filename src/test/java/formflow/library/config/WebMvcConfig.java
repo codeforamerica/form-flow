5a9c5b761312be99bcff9483209b08badede6881
chibuisienyia <cenyia@codeforamerica.org>
2023-07-17 12:52:57 -0500

    Removed zip file test
    #185447786
    
    Co-authored-by: Alex Gonzalez <agonzalez@codeforamerica.org>

--- src/test/java/formflow/library/config/WebMvcConfig.java
+++ src/test/java/formflow/library/config/WebMvcConfig.java
@@ -1,23 +1,23 @@
-package formflow.library.config;
-
-import java.util.concurrent.Executor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.AsyncConfigurer;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-@Configuration
-@EnableAsync
-public class WebMvcConfig implements AsyncConfigurer {
-
-  @Override
-  public Executor getAsyncExecutor() {
-    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-    executor.setCorePoolSize(5);  // sets the number of threads to keep in the pool
-    executor.setMaxPoolSize(10);  // sets the maximum allowed number of threads
-    executor.setQueueCapacity(100);  // sets the queue capacity for tasks
-    executor.setThreadNamePrefix("AsyncTaskExecutor-");  // sets the prefix of the names for new threads
-    executor.initialize();
-    return executor;
-  }
-}
+//package formflow.library.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+//import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebMvcConfig implements WebMvcConfigurer {
+//
+//  @Bean
+//  public ThreadPoolTaskExecutor mvcTaskExecutor() {
+//    ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+//    taskExecutor.setCorePoolSize(10);
+//    taskExecutor.setMaxPoolSize(10);
+//    return taskExecutor;
+//  }
+//
+//  public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+//    configurer.setTaskExecutor(mvcTaskExecutor());
+//  }
+//}
