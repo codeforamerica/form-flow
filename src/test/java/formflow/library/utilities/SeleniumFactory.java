1ac73fc8a4a30441607cbd4a315708976351f722
Bethany Seeger <bseeger@codeforamerica.org>
2023-07-20 14:20:13 -0400

    Test out theory of telling the driver where the executable is.

--- src/test/java/formflow/library/utilities/SeleniumFactory.java
+++ src/test/java/formflow/library/utilities/SeleniumFactory.java
@@ -3,6 +3,7 @@ package formflow.library.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import java.nio.file.Path;
 import java.util.HashMap;
+import java.util.Optional;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -36,13 +37,29 @@ public class SeleniumFactory implements FactoryBean<RemoteWebDriver> {
     // Warning: you may need this line to get a later version of the chromedriver.
     // once you have the version, you don't seem to need to specify it anymore.
     // (or maybe you can upgrade your driver outside of this)
-    // WebDriverManager.chromedriver().driverVersion("111.0.5563.64").setup();
-    WebDriverManager.chromedriver().driverVersion("114.0.5735.90").setup();
-    //WebDriverManager.chromedriver().setup();
+
+    //WebDriverManager.chromedriver().driverVersion("114.0.5735.90").setup();
+    //WebDriverManager.chromedriver().browserVersion(WebDriverManager.chromedriver().getDownloadedDriverVersion());
+
+
+    /*
+    if (browserPath.isPresent()) {
+      WebDriverManager
+          .chromedriver()
+          .browserVersionDetectionCommand(browserPath.get() + " -- version")
+          .setup();
+    } else {
+      // will grab the latest version if the binary is not found on system
+      WebDriverManager.chromedriver().setup();
+    }
+    */
+    Optional<Path> browserPath = WebDriverManager.chromedriver().getBrowserPath();
+    WebDriverManager.chromedriver().setup();
     ChromeOptions options = new ChromeOptions();
     HashMap<String, Object> chromePrefs = new HashMap<>();
     chromePrefs.put("download.default_directory", tempdir.toString());
     options.setExperimentalOption("prefs", chromePrefs);
+    options.setBinary(browserPath.get().toFile());
     options.addArguments("--window-size=1280,1600");
     options.addArguments("--headless=new");
     options.addArguments("--remote-allow-origins=*");
