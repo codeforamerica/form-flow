4fb2a28e07c16f2fb1bfb80398e2fac1f5757733
spokenbird <agonzalez@codeforamerica.org>
2023-08-17 14:02:12 -0700

    Stop cache from keeping continue button disabled on back
    Co-authored-by: Bethany Seeger <bseeger@codeforamerica.org>

--- src/main/resources/META-INF/resources/webjars/form-flow/0.0.1/js/formFlowDisableMultipleFormSubmit.js
+++ src/main/resources/META-INF/resources/webjars/form-flow/0.0.1/js/formFlowDisableMultipleFormSubmit.js
@@ -10,3 +10,14 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 });
+
+window.addEventListener('pageshow', function (event) {
+  if (event.persisted) {
+    console.log("pageshow event: the page was restored from the bfcache" + event.persisted)
+    const formSubmitButton = document.getElementById("form-submit-button");
+    if (formSubmitButton) {
+      formSubmitButton.classList.remove("button--disabled");
+      formSubmitButton.disabled = false;
+    }
+  }
+});
