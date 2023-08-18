44bc29d7a97ea88d36162fa2709b943cf1e3cf73
Alex Gonzalez <spokenbird@gmail.com>
2023-08-17 17:00:44 -0700

    Stop cache from keeping continue button disabled on back (#349)
    * Stop cache from keeping continue button disabled on back
    
    Co-authored-by: Bethany Seeger <bseeger@codeforamerica.org>

--- src/main/resources/META-INF/resources/webjars/form-flow/0.0.1/js/formFlowDisableMultipleFormSubmit.js
+++ src/main/resources/META-INF/resources/webjars/form-flow/0.0.1/js/formFlowDisableMultipleFormSubmit.js
@@ -10,3 +10,16 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 });
+
+// This prevents the button from being disabled when the user navigates back to 
+// the page after submitting the form using back button. 
+// (Safari caches the page in it's back-forward cache).
+window.addEventListener('pageshow', function (event) {
+  if (event.persisted) {
+    const formSubmitButton = document.getElementById("form-submit-button");
+    if (formSubmitButton) {
+      formSubmitButton.classList.remove("button--disabled");
+      formSubmitButton.disabled = false;
+    }
+  }
+});
