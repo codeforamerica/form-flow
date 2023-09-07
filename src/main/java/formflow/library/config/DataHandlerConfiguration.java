35b363d19448a7ba362ab5e19f392b66778e182a
spokenbird <agonzalez@codeforamerica.org>
2023-09-07 14:15:02 -0700

    Do not load Data Interceptor if application property missing
    Co-authored-by: Bethany Seeger <bseeger@codeforamerica.org>
    Co-authored-by: Cypress Borg <cborg@codeforamerica.org>
    Co-authored-by: Chibuisi Enyia <cenyia@codeforamerica.org>

--- src/main/java/formflow/library/config/DataHandlerConfiguration.java
+++ src/main/java/formflow/library/config/DataHandlerConfiguration.java
@@ -3,18 +3,21 @@ package formflow.library.config;
 import formflow.library.interceptors.DataRequiredInterceptor;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@ConditionalOnProperty(name = "form-flow.session-continuity-interceptor.enabled", havingValue = "true")
 public class DataHandlerConfiguration implements WebMvcConfigurer {
   
   @Autowired
   List<FlowConfiguration> flowConfigurations;
   
   @Override
-  public void addInterceptors(InterceptorRegistry registry){
+  public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(new DataRequiredInterceptor(flowConfigurations)).addPathPatterns(List.of(DataRequiredInterceptor.FLOW_PATH_FORMAT, DataRequiredInterceptor.NAVIGATION_FLOW_PATH_FORMAT));
   }
 }
