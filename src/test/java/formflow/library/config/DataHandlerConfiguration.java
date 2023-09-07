2aab7f01e2bf768fecaced9e78c0a78843e9c218
chibuisienyia <cenyia@codeforamerica.org>
2023-09-06 19:05:04 -0500

    Updated landmark logic to throw errors when implemented incorrectly #185865123
    Co-authored-by: Ben Calegari <bcalegari@codeforamerica.org>
    Co-authored-by: Alex Gonzalez <agonzalez@codeforamerica.org>

--- src/test/java/formflow/library/config/DataHandlerConfiguration.java
+++ src/test/java/formflow/library/config/DataHandlerConfiguration.java
@@ -1,6 +1,5 @@
 package formflow.library.config;
 
-import formflow.library.data.SubmissionRepositoryService;
 import formflow.library.interceptors.DataRequiredInterceptor;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
