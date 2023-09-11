e6658dedc018510bdec866e4bc2a20f618739f37
spokenbird <agonzalez@codeforamerica.org>
2023-09-11 14:57:15 -0700

    Add setup to prevent flakyness in tests
    Co-authored-by: Chibuisi Enyia <cenyia@codeforamerica.org>

--- src/test/java/formflow/library/interceptors/InterceptorMockMvcTest.java
+++ src/test/java/formflow/library/interceptors/InterceptorMockMvcTest.java
@@ -15,6 +15,7 @@ import formflow.library.exceptions.LandmarkNotSetException;
 import formflow.library.utilities.AbstractMockMvcTest;
 import java.util.HashMap;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InOrder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,16 +28,38 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 @SpringBootTest(properties = {"form-flow.path=flows-config/test-landmark-flow.yaml"})
 @TestPropertySource(properties = {"form-flow.session-continuity-interceptor.enabled=true"})
 @Import(SpyInterceptorConfig.class)
-class InterceptorOrderMockMvcTest extends AbstractMockMvcTest {
+class InterceptorMockMvcTest extends AbstractMockMvcTest {
   
   @Autowired
   private LocaleChangeInterceptor localeChangeInterceptor;
   
   @Autowired
   private DataRequiredInterceptor dataRequiredInterceptor;
+
+  @Override
+  @BeforeEach
+  protected void setUp() throws Exception {
+    // These tests dirty the context of the data, so we need to reset it before each test.
+    // We do this rather than use @DirtiesContext because it's much faster.
+    dataRequiredInterceptor.flowConfigurations = List.of();
+    super.setUp();
+  }
   
   @Test
   void shouldRunTheDataRequiredInterceptorLast() throws Exception {
+    FlowConfiguration flowConfiguration = new FlowConfiguration();
+    ScreenNavigationConfiguration screenNavigationConfiguration = new ScreenNavigationConfiguration();
+    NextScreen nextScreen = new NextScreen();
+    nextScreen.setName("first");
+    screenNavigationConfiguration.setNextScreens(List.of(nextScreen));
+    HashMap<String, ScreenNavigationConfiguration> screenNavigationConfigurations = new HashMap<>();
+    screenNavigationConfigurations.put("first", screenNavigationConfiguration);
+    flowConfiguration.setFlow(screenNavigationConfigurations);
+    flowConfiguration.setName("testLandmarkFlow");
+    LandmarkConfiguration landmarkConfiguration = new LandmarkConfiguration();
+    landmarkConfiguration.setFirstScreen("first");
+    flowConfiguration.setLandmarks(landmarkConfiguration);
+    dataRequiredInterceptor.flowConfigurations = List.of(flowConfiguration);
     mockMvc.perform(get("/flow/testLandmarkFlow/first?lang=es"))
         .andExpect(status().isOk());
 
