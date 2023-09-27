378ae85856545fb041c0fdfd93108ffe32a85632
chibuisienyia <cenyia@codeforamerica.org>
2023-09-27 11:32:54 -0500

    Change LandmarkNotSetException to extend exception #186134647
    Co-authored-by: Bethany Seeger <bseeger@codeforamerica.org>

--- src/main/java/formflow/library/exceptions/LandmarkNotSetException.java
+++ src/main/java/formflow/library/exceptions/LandmarkNotSetException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-public class LandmarkNotSetException extends RuntimeException {
+public class LandmarkNotSetException extends Exception {
     public LandmarkNotSetException(String message) {
         super(message);
     }
