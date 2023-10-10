245e2431922d0b8ff257c101c01c7deb1fad7145
chibuisienyia <cenyia@codeforamerica.org>
2023-10-10 11:18:15 -0500

    Updated AddressParts.java #185083045

--- src/main/java/formflow/library/inputs/AddressParts.java
+++ src/main/java/formflow/library/inputs/AddressParts.java
@@ -1,10 +1,28 @@
 package formflow.library.inputs;
 
+/**
+ * Fields used to define an address fragment
+ */
 public enum AddressParts {
+  /**
+   * Street component of an address
+   */
   STREET_ADDRESS_1("StreetAddress1"),
+  /**
+   * Apartment, suite, or office number
+   */
   STREET_ADDRESS_2("StreetAddress2"),
+  /**
+   * City name
+   */
   CITY("City"),
+  /**
+   * The State value stored as the state code, a two character String, i.e. "IL", "CA", etc.
+   */
   STATE("State"),
+  /**
+   * Five or nine digit value used by the post office to facilitate the delivery of mail.
+   */
   ZIPCODE("ZipCode");
   private final String value;
 
