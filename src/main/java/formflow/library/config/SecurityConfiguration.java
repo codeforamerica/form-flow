1ba4507075cd0afb6f968416466c69d9e4f1940d
chibuisienyia <cenyia@codeforamerica.org>
2023-10-20 11:29:07 -0500

    Moved SecurityConfiguration.java to the library
    #186292877

--- src/main/java/formflow/library/config/SecurityConfiguration.java
+++ src/main/java/formflow/library/config/SecurityConfiguration.java
@@ -7,19 +7,45 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.web.filter.ForwardedHeaderFilter;
+
+/**
+ * Security Configuration
+ */
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    /**
+     * Sets session cookies to HttpOny and Secure
+     * @return serializer with updated cookie settings
+     */
+    @Bean
+    public DefaultCookieSerializer setDefaultSecurityCookie(){
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setUseSecureCookie(true);
+        serializer.setUseHttpOnlyCookie(true);
+        return serializer;
+    }
+
+    /**
+     * Disables basic auth while retaining other security features
+     * @param httpSecurity HttpSecurity object
+     * @return HttpSecurity configuration with authentication disabled
+     * @throws Exception
+     */
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.formLogin(AbstractHttpConfigurer::disable);
         return httpSecurity.build();
     }
 
+    /**
+     * Use X-Forwarded-For / X-Forwarded-Proto headers when generating full link URLs.
+     * @return ForwardedHeaderFilter object
+     */
     @Bean
-    // Use X-Forwarded-For / X-Forwarded-Proto headers when generating full link URLs.
     public ForwardedHeaderFilter forwardedHeaderFilter() {
         return new ForwardedHeaderFilter();
     }
