7b06d3f3dea279626fd4902f53d717f95681146d
chibuisienyia <cenyia@codeforamerica.org>
2023-10-20 02:34:30 -0500

    Set HttpOnly and Secure response in our session cookie (updated) #186292877

--- src/test/java/formflow/library/config/SecurityConfiguration.java
+++ src/test/java/formflow/library/config/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.session.web.http.DefaultCookieSerializer;
@@ -19,6 +18,7 @@ public class SecurityConfiguration {
     public DefaultCookieSerializer setDefaultSecurityCookie(){
         DefaultCookieSerializer serializer = new DefaultCookieSerializer();
         serializer.setUseSecureCookie(true);
+        serializer.setUseHttpOnlyCookie(true);
         return serializer;
     }
     @Bean
