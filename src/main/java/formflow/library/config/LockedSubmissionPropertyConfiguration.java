8990b1caf985a66a64638300224e14350ac1b86b
spokenbird <agonzalez@codeforamerica.org>
2023-11-06 14:53:31 -0800

    Avoid configuration property namespacing issues

--- src/main/java/formflow/library/config/LockedSubmissionPropertyConfiguration.java
+++ src/main/java/formflow/library/config/LockedSubmissionPropertyConfiguration.java
@@ -3,20 +3,17 @@ package formflow.library.config;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "form-flow.disabled-flows")
+@ConfigurationProperties(prefix = "form-flow.lock-after-submitted")
 @Getter
 @Setter
-public class DisabledFlowPropertyConfiguration {
-  private List<Map<String, String>> disabledFlows = new ArrayList<>();
+public class LockedSubmissionPropertyConfiguration {
+  private List<Map<String, String>> lockAfterSubmitted = new ArrayList<>();
 
 
   /**
@@ -24,19 +21,19 @@ public class DisabledFlowPropertyConfiguration {
    * @param flowName the name of the flow to check.
    * @return true if the flow is disabled, false otherwise.
    */
-  public boolean isFlowDisabled(String flowName) {
-    return this.disabledFlows.stream().anyMatch(flow -> flow.get("flow").equals(flowName));
+  public boolean isFlowSubmissionLocked(String flowName) {
+    return this.lockAfterSubmitted.stream().anyMatch(flow -> flow.get("flow").equals(flowName));
   }
-  
+
   /**
    * Gets the static redirect page for a disabled flow.
    * @param flowName the name of the flow to check.
    * @return the static redirect page for the flow if disabled flows are configured and the flow is disabled.
    */
-  public String getDisabledFlowRedirect(String flowName) {
-    return disabledFlows.stream()
+  public String getRedirectForSubmissionLockedFlow(String flowName) {
+    return lockAfterSubmitted.stream()
         .filter(flow -> flow.get("flow").equals(flowName))
-        .map(flow -> flow.getOrDefault("staticRedirectPage", ""))
+        .map(flow -> flow.getOrDefault("submissionLockedRedirect", ""))
         .findFirst()
         .orElse(null);
   }
