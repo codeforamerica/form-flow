2ab7411db1888b43310220677bb8f7d487c25312
Vraj Mohan <vraj@codeforamerica.org>
2023-11-15 05:49:24 -0800

    Use openpdf instead of pdfbox
    1. Font substitution is easier to implement.
    2. It is a fork of iText and has better documentation in the form of
       actual published books.
    3. It should be easy to migrate to iText from openpdf if we choose to.
    4. pdfbox 3.0 does not appear to be released frequently. It takes a long
       time and emits a lot of warnings during font loading.

--- src/test/java/formflow/library/pdf/PDFBoxFieldFillerTest.java
+++ src/test/java/formflow/library/pdf/PDFBoxFieldFillerTest.java
@@ -1,16 +1,17 @@
 package formflow.library.pdf;
 
-import org.apache.pdfbox.Loader;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
+import com.lowagie.text.pdf.AcroFields;
+import com.lowagie.text.pdf.PdfReader;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PDFBoxFieldFillerTest {
 
@@ -19,41 +20,61 @@ class PDFBoxFieldFillerTest {
 
   @Test
   void shouldMapAllFieldTypes() throws IOException {
-    String expectedFieldValue = "Michael";
+    String textFieldValue = "Michael";
     String radioValue = "option2";
     String checkboxSelectedValue = "Yes";
     String unselectedCheckboxValue = "Off";
 
     Collection<PdfField> fields = List.of(
-        new PdfField("TEXT_FIELD", expectedFieldValue),
+        new PdfField("TEXT_FIELD", textFieldValue),
         new PdfField("RADIO_BUTTON", radioValue),
         new PdfField("CHECKBOX_OPTION_1", checkboxSelectedValue),
         new PdfField("CHECKBOX_OPTION_3", checkboxSelectedValue)
     );
 
-    PdfFile pdfFile = pdfBoxFieldFiller.fill(pdf, fields);
-    PDDocument pdDocument = Loader.loadPDF(pdfFile.fileBytes());
-    PDAcroForm acroForm = pdDocument.getDocumentCatalog().getAcroForm();
+    PdfFile pdfFile = pdfBoxFieldFiller.fill(pdf, fields, false);
 
-    assertThat(acroForm.getField("TEXT_FIELD").getValueAsString()).isEqualTo(expectedFieldValue);
-    assertThat(acroForm.getField("RADIO_BUTTON").getValueAsString()).isEqualTo(radioValue);
-    assertThat(acroForm.getField("CHECKBOX_OPTION_1").getValueAsString()).isEqualTo(checkboxSelectedValue);
-    assertThat(acroForm.getField("CHECKBOX_OPTION_2").getValueAsString()).isEqualTo(unselectedCheckboxValue);
-    assertThat(acroForm.getField("CHECKBOX_OPTION_3").getValueAsString()).isEqualTo(checkboxSelectedValue);
-
-    pdDocument.close();
+    assertEquals(getField(pdfFile, "TEXT_FIELD"), textFieldValue);
+    assertEquals(getField(pdfFile, "RADIO_BUTTON"), radioValue);
+    assertEquals(getField(pdfFile, "CHECKBOX_OPTION_1"), checkboxSelectedValue);
+    assertEquals(getField(pdfFile, "CHECKBOX_OPTION_2"), unselectedCheckboxValue);
+    assertEquals(getField(pdfFile,"CHECKBOX_OPTION_3"), checkboxSelectedValue);
   }
 
   @Test
+  @Disabled
   void shouldSetNullTextFieldsAsEmptyString() throws IOException {
     Collection<PdfField> fields = List.of(
         new PdfField("TEXT_FIELD", null)
     );
     PdfFile pdfFile = pdfBoxFieldFiller.fill(pdf, fields);
 
-    PDDocument pdDocument = Loader.loadPDF(pdfFile.fileBytes());
-    assertThat(pdDocument.getDocumentCatalog().getAcroForm().getField("TEXT_FIELD").getValueAsString()).isEqualTo("");
-    pdDocument.close();
+    Assertions.assertEquals(getField(pdfFile, "TEXT_FIELD"), "");
+
+  }
+
+  @Test
+  void shouldAccept_a_with_macron() throws IOException {
+    String textFieldValue = "北京";
+    Collection<PdfField> fields = List.of(
+            new PdfField("TEXT_FIELD", textFieldValue)
+    );
+
+    PdfFile pdfFile = pdfBoxFieldFiller.fill(pdf, fields, false);
+
+    assertEquals(textFieldValue, getField(pdfFile, "TEXT_FIELD"));
+  }
+
+  @Test
+  void shouldAcceptSimplifiedChinese() throws IOException {
+    String textFieldValue = "北京";
+    Collection<PdfField> fields = List.of(
+            new PdfField("TEXT_FIELD", textFieldValue)
+    );
+
+    PdfFile pdfFile = pdfBoxFieldFiller.fill(pdf, fields, false);
+
+    assertEquals(textFieldValue, getField(pdfFile, "TEXT_FIELD"));
   }
 
   @Test
@@ -67,17 +88,19 @@ class PDFBoxFieldFillerTest {
   @Test
   void shouldSupportEmojis() throws IOException {
     String submittedValue = "Michael😃";
-    String expectedValue = "Michael😃";
 
     Collection<PdfField> fields = List.of(
         new PdfField("TEXT_FIELD", submittedValue)
     );
 
-    PdfFile pdfFile = pdfBoxFieldFiller.fill(pdf, fields);
-    PDDocument pdDocument = Loader.loadPDF(pdfFile.fileBytes());
-    PDAcroForm acroForm = pdDocument.getDocumentCatalog().getAcroForm();
+    PdfFile pdfFile = pdfBoxFieldFiller.fill(pdf, fields, false);
+    assertEquals(getField(pdfFile, "TEXT_FIELD"), submittedValue);
+  }
 
-    assertThat(acroForm.getField("TEXT_FIELD").getValueAsString()).isEqualTo(expectedValue);
-    pdDocument.close();
+  private static String getField(PdfFile pdfFile, String fieldName) throws IOException {
+    try (PdfReader reader = new PdfReader(String.join("/", pdfFile.path(), pdfFile.name()))) {
+      AcroFields form = reader.getAcroFields();
+      return form.getField(fieldName);
+    }
   }
 }
