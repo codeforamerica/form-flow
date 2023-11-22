4bce5d2a03a8f2352a3106f13ef54c872bb068cf
chibuisienyia <cenyia@codeforamerica.org>
2023-11-22 11:08:00 -0600

    Implemented change that substitutes unsupported characters with question marks and prints pdf to a screen #186487626
    Co-authored-by: Martha Pidcock <mpidcock@codeforamerica.org>

--- src/main/java/formflow/library/pdf/PDFBoxFieldFiller.java
+++ src/main/java/formflow/library/pdf/PDFBoxFieldFiller.java
@@ -1,7 +1,14 @@
 package formflow.library.pdf;
 
+import java.util.List;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDTrueTypeFont;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
 import org.apache.pdfbox.pdmodel.interactive.form.PDCheckBox;
 import org.apache.pdfbox.pdmodel.interactive.form.PDField;
@@ -70,9 +77,31 @@ public class PDFBoxFieldFiller {
       }
       pdField.setValue(field);
     } catch (IllegalArgumentException e) {
+      substituteUnsupportedCharactersWithQuestionMarks(field, pdField);
       log.error(
           "Error setting value '%s' for field %s".formatted(field,
               pdField.getFullyQualifiedName()));
     }
   }
+
+  private void substituteUnsupportedCharactersWithQuestionMarks(String field, PDField pdField) throws IOException {
+    StringBuilder builder = new StringBuilder();
+    Iterable<COSName> fontNames = pdField.getAcroForm().getDefaultResources().getFontNames();
+    for (COSName cosName: fontNames){
+      PDType1Font font = (PDType1Font) pdField.getAcroForm().getDefaultResources().getFont(cosName);
+      if (builder.isEmpty()){
+        for(int i = 0; i < field.length(); i++){
+          Character character = field.charAt(i);
+          if(font.hasGlyph(character)){
+            builder.append(character);
+          }else{
+            builder.append('?');
+          }
+        }
+      }
+      System.out.println("test");
+      pdField.setValue(builder.toString());
+    }
+  }
+
 }
