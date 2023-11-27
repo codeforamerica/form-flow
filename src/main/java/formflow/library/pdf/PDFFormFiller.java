30b9cffcf80ff139fcab34c05ba5f3c0300a009c
Vraj Mohan <vraj@codeforamerica.org>
2023-11-27 15:57:54 -0800

    Load the font files found under `resources/pdf-fonts`
    Co-authored-by: Alex Gonzalez <agonzalez@codeforamerica.org>
    
    Use them as substitution fonts

--- src/main/java/formflow/library/pdf/PDFFormFiller.java
+++ src/main/java/formflow/library/pdf/PDFFormFiller.java
@@ -5,12 +5,18 @@ import com.lowagie.text.pdf.BaseFont;
 import com.lowagie.text.pdf.PdfReader;
 import com.lowagie.text.pdf.PdfStamper;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URI;
 import java.util.Collection;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Slf4j
@@ -29,7 +35,7 @@ public class PDFFormFiller {
       File outputFile = File.createTempFile("Filled_", ".pdf");
       PdfStamper pdfStamper = new PdfStamper(reader, new FileOutputStream(outputFile));
       AcroFields acroFields = pdfStamper.getAcroFields();
-      configureSubstituteFonts(acroFields);
+      configureSubstitutionFonts(acroFields);
       for (PdfField pdfField : fields) {
         acroFields.setField(pdfField.name(), pdfField.value());
       }
@@ -57,8 +63,9 @@ public class PDFFormFiller {
    * Configure the fonts to be used if the default fonts do not support the characters in the values to be filled
    * @param acroFields the `AcroFields` object (the form) in the PDF template
    */
-  private static void configureSubstituteFonts(AcroFields acroFields) {
-    Stream.of("NotoSans-Regular.ttf", "NotoSansSC-Regular.ttf").forEach( fontResource -> {
+  private static void configureSubstitutionFonts(AcroFields acroFields) {
+    Set<String> files = getFontFiles();
+    files.forEach(fontResource -> {
       BaseFont font = null;
       try {
         font = BaseFont.createFont(fontResource, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
@@ -67,5 +74,24 @@ public class PDFFormFiller {
       }
       acroFields.addSubstitutionFont(font);
     });
+  }
+
+  /**
+   * Get the list of font files to be used as substitution fonts from resources/pdf-fonts
+   * @return Set of filenames with paths
+   */
+  @NotNull
+  private static Set<String> getFontFiles() {
+    URI directoryURI = null;
+    try {
+      directoryURI = new ClassPathResource("pdf-fonts").getURI();
+    } catch (IOException e) {
+      log.warn("Failed to find pdf-fonts directory: %s", e);
+    }
+
+    return Stream.of(Objects.requireNonNull(new File(directoryURI).listFiles()))
+            .filter(file -> !file.isDirectory())
+            .map(File::getPath)
+            .collect(Collectors.toSet());
   }
 }
