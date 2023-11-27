be26ea9d2cfbb6a08c4f1ddb08f90acb7c72ba85
Alex Gonzalez <spokenbird@gmail.com>
2023-11-27 10:10:09 -0800

    Do not require DB fields in pdf-map (#449)
    [#186402592]

--- src/main/java/formflow/library/pdf/DatabaseFieldPreparer.java
+++ src/main/java/formflow/library/pdf/DatabaseFieldPreparer.java
@@ -17,20 +17,23 @@ public class DatabaseFieldPreparer implements DefaultSubmissionFieldPreparer {
   public Map<String, SubmissionField> prepareSubmissionFields(Submission submission, PdfMap pdfMap) {
     Map<String, SubmissionField> databaseFields = new HashMap<>();
     Map<String, Object> dbFields = pdfMap.getDbFields();
-
-    dbFields.forEach((fieldName, value) -> {
-      switch (fieldName) {
-        case "submittedAt" ->
-            databaseFields.put(fieldName, new DatabaseField(fieldName, formatDateWithNoTime(submission.getSubmittedAt())));
-        case "submissionId" -> databaseFields.put(fieldName, new DatabaseField(fieldName, submission.getId().toString()));
-        case "createdAt" ->
-            databaseFields.put(fieldName, new DatabaseField(fieldName, formatDateWithNoTime(submission.getCreatedAt())));
-        case "updatedAt" ->
-            databaseFields.put(fieldName, new DatabaseField(fieldName, formatDateWithNoTime(submission.getUpdatedAt())));
-        case "flow" -> databaseFields.put(fieldName, new DatabaseField(fieldName, submission.getFlow()));
-        default -> log.error("Unable to map unknown database field: {}", fieldName);
-      }
-    });
+    
+    if (dbFields != null) {
+      dbFields.forEach((fieldName, value) -> {
+        switch (fieldName) {
+          case "submittedAt" ->
+              databaseFields.put(fieldName, new DatabaseField(fieldName, formatDateWithNoTime(submission.getSubmittedAt())));
+          case "submissionId" -> databaseFields.put(fieldName, new DatabaseField(fieldName, submission.getId().toString()));
+          case "createdAt" ->
+              databaseFields.put(fieldName, new DatabaseField(fieldName, formatDateWithNoTime(submission.getCreatedAt())));
+          case "updatedAt" ->
+              databaseFields.put(fieldName, new DatabaseField(fieldName, formatDateWithNoTime(submission.getUpdatedAt())));
+          case "flow" -> databaseFields.put(fieldName, new DatabaseField(fieldName, submission.getFlow()));
+          default -> log.error("Unable to map unknown database field: {}", fieldName);
+        }
+      });
+    }
+    
     return databaseFields;
   }
 
