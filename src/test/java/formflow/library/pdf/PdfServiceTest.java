bc0acacbf5bab6cadb775b5ac8b30329d242630f
Vraj Mohan <vraj@codeforamerica.org>
2023-11-28 14:29:28 -0800

    Format with 2 space indents to conform to project style

--- src/test/java/formflow/library/pdf/PdfServiceTest.java
+++ src/test/java/formflow/library/pdf/PdfServiceTest.java
@@ -18,31 +18,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ActiveProfiles("test")
 @SpringBootTest(properties = {"form-flow.path=flows-config/test-flow.yaml"})
 class PdfServiceTest {
-    @Autowired
-    PdfService pdfService;
 
-    @Test
-    void generate() throws IOException {
-        Map<String, Object> inputData = Map.of(
-                "firstName", "Michaelā",
-                "city", "San Francisco"
-        );
-        Submission submission = Submission.builder()
-                .flow("ubi")
-                .submittedAt((DateTime.parse("2023-11-28").toDate()))
-                .inputData(inputData)
-                .build();
-        File pdfFile = pdfService.generate(submission);
-        String text = getText(pdfFile);
-        assertThat(text).contains("San Francisco");
-        assertThat(text).contains("Michael ā"); // Extra space to account for idiosyncrasies in text extraction from PDFs
-        assertThat(text).contains("11/28/2023");
-    }
+  @Autowired
+  PdfService pdfService;
 
-    private static String getText(File file) throws IOException {
-        try (PdfReader reader = new PdfReader(file.getPath())) {
-            PdfTextExtractor pdfTextExtractor = new PdfTextExtractor(reader);
-            return pdfTextExtractor.getTextFromPage(1);
-        }
+  @Test
+  void generate() throws IOException {
+    Map<String, Object> inputData = Map.of(
+        "firstName", "Michaelā",
+        "city", "San Francisco"
+    );
+    Submission submission = Submission.builder()
+        .flow("ubi")
+        .submittedAt((DateTime.parse("2023-11-28").toDate()))
+        .inputData(inputData)
+        .build();
+    File pdfFile = pdfService.generate(submission);
+    String text = getText(pdfFile);
+    assertThat(text).contains("San Francisco");
+    assertThat(text).contains("Michael ā"); // Extra space to account for idiosyncrasies in text extraction from PDFs
+    assertThat(text).contains("11/28/2023");
+  }
+
+  private static String getText(File file) throws IOException {
+    try (PdfReader reader = new PdfReader(file.getPath())) {
+      PdfTextExtractor pdfTextExtractor = new PdfTextExtractor(reader);
+      return pdfTextExtractor.getTextFromPage(1);
     }
+  }
 }
