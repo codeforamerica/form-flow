beba43cdb413a426aa32700fd57ca724072c6fbe
Cypress Borg <9101728+coltborg@users.noreply.github.com>
2023-12-08 13:35:14 -0600

    Adding javadocs to some classes (#463)
    * Adding javadocs to some classes
    - address validation
    - starting custom annotations
    - annotation classes
    - started validators
    - formSubmission
    - dynamic field
    - form submission
    - library/utils
    - one more validator
    - disabled flow
    - form flow exception
    - form flow configuration properties
    - Be more descriptive ijn FormSubmission
    - add regex reference in RegexUtils

--- src/main/java/formflow/library/utils/RegexUtils.java
+++ src/main/java/formflow/library/utils/RegexUtils.java
@@ -1,6 +1,18 @@
 package formflow.library.utils;
 
+/**
+ * Utility class containing regular expressions for various patterns. This class serves as a centralized repository for regex
+ * patterns used throughout the application.
+ */
 public class RegexUtils {
 
+  /**
+   * Regular expression pattern for validating email addresses. This pattern conforms to the general email format, allowing a wide
+   * range of email addresses. It includes support for various characters in the local part and domain of the email address.
+   *
+   * @see <a
+   * href="https://github.com/codeforamerica/shiba/blob/f3f241e01cf72587813f312c086b557c2412210e/src/main/java/org/codeforamerica/shiba/pages/config/Validation.java#L59-L60">Email
+   * regex is from Shiba.</a>
+   */
   public static final String EMAIL_REGEX = "[a-zA-Z0-9!#$%&'*+=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&'*+=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?";
 }
