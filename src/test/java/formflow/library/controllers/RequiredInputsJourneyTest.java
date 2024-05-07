807638287b542ee2291e108cf11787a37505ee3f
spokenbird <agonzalez@codeforamerica.org>
2024-05-07 12:59:59 -0700

    Await client to see if it fixes CI

--- src/test/java/formflow/library/controllers/RequiredInputsJourneyTest.java
+++ src/test/java/formflow/library/controllers/RequiredInputsJourneyTest.java
@@ -1,39 +1,43 @@
 package formflow.library.controllers;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 import formflow.library.utilities.AbstractBasePageTest;
 import java.io.IOException;
+import java.time.Duration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {"form-flow.path=flows-config/test-required-inputs-flow.yaml"},
-    webEnvironment = RANDOM_PORT)
+        webEnvironment = RANDOM_PORT)
 public class RequiredInputsJourneyTest extends AbstractBasePageTest {
 
+  private WebDriverWait wait;
 
-    @Override
+  @Override
   @BeforeEach
   public void setUp() throws IOException {
-        String testFlow = "requiredInputs";
-        startingPage = "flow/" + testFlow + "/inputs";
+    String testFlow = "requiredInputs";
+    startingPage = "flow/" + testFlow + "/inputs";
     super.setUp();
+    wait = new WebDriverWait(driver, Duration.ofSeconds(3));  // setting up a wait of 3 seconds
   }
 
   @Test
   public void automaticallyAppendsRequiredToAppropriateFields() {
-    assertThat(driver.findElement(By.ByCssSelector.cssSelector("label[for='textInput']")).getText()).contains("(required)");
-    assertThat(driver.findElement(By.ByCssSelector.cssSelector("label[for='areaInput']")).getText()).contains("(required)");
-    assertThat(driver.findElement(By.ByCssSelector.cssSelector("label[for='numberInput']")).getText()).contains("(required)");
-    assertThat(driver.findElement(By.ByCssSelector.cssSelector("label[for='selectInput']")).getText()).contains("(required)");
-    assertThat(driver.findElement(By.ByCssSelector.cssSelector("label[for='moneyInput']")).getText()).contains("(required)");
-    assertThat(driver.findElement(By.ByCssSelector.cssSelector("label[for='phoneInput']")).getText()).contains("(required)");
-    assertThat(driver.findElement(By.ByCssSelector.cssSelector("label[for='ssnInput']")).getText()).contains("(required)");
-    assertThat(driver.findElement(By.ByCssSelector.cssSelector("legend[for='date']")).getText()).contains("(required)");
-    assertThat(driver.findElement(By.id("checkboxSet-legend")).getText()).contains("(required)");
-    assertThat(driver.findElement(By.id("radioInput-legend")).getText()).contains("(required)");
+    wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("label[for='textInput']"), "(required)"));
+    wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("label[for='areaInput']"), "(required)"));
+    wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("label[for='numberInput']"), "(required)"));
+    wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("label[for='selectInput']"), "(required)"));
+    wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("label[for='moneyInput']"), "(required)"));
+    wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("label[for='phoneInput']"), "(required)"));
+    wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("label[for='ssnInput']"), "(required)"));
+    wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("legend[for='date']"), "(required)"));
+    wait.until(ExpectedConditions.textToBePresentInElementLocated(By.id("checkboxSet-legend"), "(required)"));
+    wait.until(ExpectedConditions.textToBePresentInElementLocated(By.id("radioInput-legend"), "(required)"));
   }
 }
