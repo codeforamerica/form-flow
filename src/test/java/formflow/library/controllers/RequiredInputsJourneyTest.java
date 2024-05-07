be1ed9e6307f3e980d34ed64f020fc851572294d
spokenbird <agonzalez@codeforamerica.org>
2024-05-07 14:34:12 -0700

    Try yet anothing thing

--- src/test/java/formflow/library/controllers/RequiredInputsJourneyTest.java
+++ src/test/java/formflow/library/controllers/RequiredInputsJourneyTest.java
@@ -1,49 +1,40 @@
 package formflow.library.controllers;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.awaitility.Awaitility.await;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 import formflow.library.utilities.AbstractBasePageTest;
 import java.io.IOException;
-import java.time.Duration;
-import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {"form-flow.path=flows-config/test-required-inputs-flow.yaml"},
-        webEnvironment = RANDOM_PORT)
+    webEnvironment = RANDOM_PORT)
 public class RequiredInputsJourneyTest extends AbstractBasePageTest {
 
-  private WebDriverWait wait;
 
-  @Override
+    @Override
   @BeforeEach
   public void setUp() throws IOException {
-    String testFlow = "requiredInputs";
-    startingPage = "flow/" + testFlow + "/inputs";
+        String testFlow = "requiredInputs";
+        startingPage = "flow/" + testFlow + "/inputs";
     super.setUp();
   }
 
   @Test
   public void automaticallyAppendsRequiredToAppropriateFields() {
-    await().until(
-            () -> !driver.findElements(By.className("main-footer")).get(0).getAttribute("innerHTML")
-                    .isBlank());
-
-    List<String> inputs = List.of(
-            "textInput",
-            "areaInput",
-            "selectInput",
-            "moneyInput",
-            "phoneInput",
-            "ssnInput");
-    inputs.forEach(inputName -> {
-      assertThat(driver.findElement(By.cssSelector(String.format("label[for='%s']", inputName))).getText()).contains("(required)");
-    });
+    assertThat(driver.findElement(By.cssSelector("label[for='textInput']")).findElement(By.className("required-input")).getText()).contains("(required)");
+    assertThat(driver.findElement(By.cssSelector("label[for='areaInput']")).findElement(By.className("required-input")).getText()).contains("(required)");
+    assertThat(driver.findElement(By.cssSelector("label[for='numberInput']")).findElement(By.className("required-input")).getText()).contains("(required)");
+    assertThat(driver.findElement(By.cssSelector("label[for='selectInput']")).findElement(By.className("required-input")).getText()).contains("(required)");
+    assertThat(driver.findElement(By.cssSelector("label[for='moneyInput']")).findElement(By.className("required-input")).getText()).contains("(required)");
+    assertThat(driver.findElement(By.cssSelector("label[for='phoneInput']")).findElement(By.className("required-input")).getText()).contains("(required)");
+    assertThat(driver.findElement(By.cssSelector("label[for='ssnInput']")).findElement(By.className("required-input")).getText()).contains("(required)");
+    assertThat(driver.findElement(By.cssSelector("legend[for='date']")).findElement(By.className("required-input")).getText()).contains("(required)");
+    assertThat(driver.findElement(By.id("checkboxSet-legend")).findElement(By.className("required-input")).getText()).contains("(required)");
+    assertThat(driver.findElement(By.id("radioInput-legend")).findElement(By.className("required-input")).getText()).contains("(required)");
   }
+
 }
