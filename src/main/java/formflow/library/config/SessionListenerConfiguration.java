7043a72c7833dfee1265d01b564c70eecea1390e
spokenbird <agonzalez@codeforamerica.org>
2024-06-03 18:58:53 -0700

    Remove unused imports

--- src/main/java/formflow/library/config/SessionListenerConfiguration.java
+++ src/main/java/formflow/library/config/SessionListenerConfiguration.java
@@ -1,13 +1,10 @@
 package formflow.library.config;
 
 import formflow.library.listeners.SessionListenerLogger;
-import java.util.Collections;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
-import org.springframework.session.web.http.SessionEventHttpSessionListenerAdapter;
 
 @Configuration
 @Slf4j
