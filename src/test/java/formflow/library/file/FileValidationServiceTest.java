21ccf612dd93183ad6d40bfc14e12519c38ce148
spokenbird <agonzalez@codeforamerica.org>
2024-08-29 14:26:16 -0700

    Fix failing tests 5

--- src/test/java/formflow/library/file/FileValidationServiceTest.java
+++ src/test/java/formflow/library/file/FileValidationServiceTest.java
@@ -53,7 +53,7 @@ class FileValidationServiceTest {
 
   @Test
   void detectsFalseMimeTypes() throws IOException {
-    ClassPathResource resource = new ClassPathResource("I-am-not-a-png.txt.png");
+    ClassPathResource resource = new ClassPathResource("i-am-not-a-png.txt.png");
     MultipartFile file = new MockMultipartFile("file", "I-am-not-a-png.txt.png", MediaType.IMAGE_PNG_VALUE, resource.getInputStream());
     FileValidationService fileValidationService = new FileValidationService(".jpeg,.bmp", 1);
     assertThat(fileValidationService.isAcceptedMimeType(file)).isFalse();
