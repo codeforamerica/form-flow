92f42e8437bf9aded1fc31346e8dd8b3901186ef
Marc Perlman <mperlman@codeforamerica.org>
2025-02-10 15:47:21 -0600

    [CCAP-659] Adding a couple User File methods to look up by mimetype and sorting by original name

--- src/main/java/formflow/library/data/UserFileRepository.java
+++ src/main/java/formflow/library/data/UserFileRepository.java
@@ -12,22 +12,47 @@ import java.util.UUID;
 @Repository
 public interface UserFileRepository extends JpaRepository<UserFile, UUID> {
 
-  /**
-   * Finds all the {@link UserFile}s associated with a {@link Submission}}
-   *
-   * @param submission the {@link Submission} for which the associated {@link UserFile}s are sought
-   * @return {@link List} of associated {@link UserFile} objects
-   */
-  List<UserFile> findAllBySubmission(Submission submission);
+    /**
+     * Finds all the {@link UserFile}s associated with a {@link Submission}}
+     *
+     * @param submission the {@link Submission} for which the associated {@link UserFile}s are sought
+     * @return {@link List} of associated {@link UserFile} objects
+     */
+    List<UserFile> findAllBySubmission(Submission submission);
 
-  /**
-   * Gets a count of all the {@link UserFile}s associated with a {@link Submission}} where the
-   * file has never been converted
-   *
-   * @param submission the {@link Submission} for which the count of associated {@link UserFile}s are sought
-   * @return count of {@link UserFile}s
-   */
-  long countBySubmissionAndConversionSourceFileIdIsNull(Submission submission);
+    /**
+     * Gets a count of all the {@link UserFile}s associated with a {@link Submission}} where the file has never been converted
+     *
+     * @param submission the {@link Submission} for which the count of associated {@link UserFile}s are sought
+     * @return count of {@link UserFile}s
+     */
+    long countBySubmissionAndConversionSourceFileIdIsNull(Submission submission);
 
-  List<UserFile> findAllBySubmissionAndConversionSourceFileId(Submission submission, UUID conversionSourceFileId);
+    /**
+     * Finds all the {@link UserFile}s associated with a {@link Submission}} where the conversionSourceFileId matches
+     *
+     * @param submission the {@link Submission} for which the associated {@link UserFile}s are sought
+     * @param conversionSourceFileId
+     * @return
+     */
+    List<UserFile> findAllBySubmissionAndConversionSourceFileId(Submission submission, UUID conversionSourceFileId);
+
+    /**
+     * Finds all the {@link UserFile}s associated with a {@link Submission}} ordered by the OriginalName field
+     *
+     * @param submission the {@link Submission} for which the associated {@link UserFile}s are sought
+     * @return
+     */
+    List<UserFile> findAllBySubmissionOrderByOriginalName(Submission submission);
+
+    /**
+     * Finds all the {@link UserFile}s associated with a {@link Submission}} where the mimeType matches and ordered by the
+     * OriginalName field
+     *
+     * @param submission the {@link Submission} for which the associated {@link UserFile}s are sought
+     * @param mimeType
+     * @return
+     */
+    List<UserFile> findAllBySubmissionAndMimeTypeOrderByOriginalName(Submission submission, String mimeType);
+
 }
