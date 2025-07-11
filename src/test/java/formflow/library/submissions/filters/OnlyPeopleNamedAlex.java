8d717d91a3d0df3c96d5418c8e241b08ec9cdc61
Marc Perlman <mperlman@codeforamerica.org>
2025-07-11 17:11:15 -0500

    Fixing test

--- src/test/java/formflow/library/submissions/filters/OnlyPeopleNamedAlex.java
+++ src/test/java/formflow/library/submissions/filters/OnlyPeopleNamedAlex.java
@@ -1,6 +1,7 @@
 package formflow.library.submissions.filters;
 
 import formflow.library.config.submission.SubflowRelationshipFilter;
+import formflow.library.data.Submission;
 import java.util.HashMap;
 import java.util.List;
 import org.springframework.stereotype.Component;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Component;
 public class OnlyPeopleNamedAlex implements SubflowRelationshipFilter {
     
     @Override
-    public List<HashMap<String, Object>> filter(List<HashMap<String, Object>> subflowDataToFilter) {
+    public List<HashMap<String, Object>> filter(List<HashMap<String, Object>> subflowDataToFilter, Submission submission) {
         return subflowDataToFilter.stream().filter(data -> "Alex".equals(data.get("householdMemberFirstName"))).toList();
     }
 }
