b4aaa8221a2937f7520d695b9a2768e1c5c383c5
Marc P <mperlman@codeforamerica.org>
2025-12-12 09:26:18 -0600

    [FFB-4] Removing Mailgun from Form Flow (#994)

--- src/test/java/formflow/library/framework/AfterSaveActionTest.java
+++ src/test/java/formflow/library/framework/AfterSaveActionTest.java
@@ -2,28 +2,22 @@ package formflow.library.framework;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.mailgun.api.v3.MailgunMessagesApi;
-import com.mailgun.model.message.Message;
 import formflow.library.ScreenController;
 import formflow.library.data.Submission;
+import formflow.library.data.SubmissionRepository;
 import formflow.library.data.SubmissionRepositoryService;
-import formflow.library.email.MailgunEmailClient;
 import formflow.library.utilities.AbstractMockMvcTest;
 import java.util.HashMap;
 import java.util.Optional;
 import java.util.UUID;
-import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -33,53 +27,36 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 @SpringBootTest(properties = {"form-flow.path=flows-config/test-after-save-action.yaml"})
 public class AfterSaveActionTest extends AbstractMockMvcTest {
 
-    private final MailgunMessagesApi mailgunMessagesApi = mock(MailgunMessagesApi.class);
     Submission submission;
+
     @MockitoBean
     private SubmissionRepositoryService submissionRepositoryService;
+
+    @MockitoBean
+    private SubmissionRepository submissionRepository;
+
     @Autowired
     private ScreenController screenController;
-    @Autowired
-    private MailgunEmailClient mailgunEmailClient;
 
     @BeforeEach
     public void setUp() throws Exception {
         UUID submissionUUID = UUID.randomUUID();
         mockMvc = MockMvcBuilders.standaloneSetup(screenController).build();
         submission = Submission.builder().id(submissionUUID).inputData(new HashMap<>()).build();
-        mailgunEmailClient.setMailgunMessagesApi(mailgunMessagesApi);
 
         super.setUp();
-        ////when(submissionRepositoryService.findOrCreate(any())).thenReturn(submission);
         when(submissionRepositoryService.findById(any())).thenReturn(Optional.of(submission));
     }
 
     @Test
-    void shouldSendEmailAfterSave() throws Exception {
-        final ArgumentCaptor<Message> captor = ArgumentCaptor.forClass(Message.class);
-        doReturn(null).when(mailgunMessagesApi).sendMessage(any(), captor.capture());
-        String expectedSubject = "Subject";
-        String expectedRecipient = "test@example.com";
-        String expectedBody = "This is a test email";
-
+    void callsAfterSaveActionAfterSave() throws Exception {
         postWithoutData("testFlow", "inputs").andExpect(redirectedUrl(getUrlForPageName("testFlow", "inputs") + "/navigation"));
 
-        verify(mailgunMessagesApi, times(1)).sendMessage(any(), any());
-
-        final Message builtMessage = captor.getValue();
-        AssertionsForClassTypes.assertThat(builtMessage).isNotNull();
-        AssertionsForClassTypes.assertThat(builtMessage.getSubject()).isEqualTo(expectedSubject);
-        AssertionsForClassTypes.assertThat(builtMessage.getTo().contains(expectedRecipient)).isEqualTo(true);
-        AssertionsForClassTypes.assertThat(builtMessage.getHtml()).isEqualTo(expectedBody);
+        verify(submissionRepository, times(1)).findById(any(UUID.class));
     }
 
     @Test
-    void shouldSendEmailAfterSaveInSubflow() throws Exception {
-        final ArgumentCaptor<Message> captor = ArgumentCaptor.forClass(Message.class);
-        doReturn(null).when(mailgunMessagesApi).sendMessage(any(), captor.capture());
-        String expectedSubject = "Subject";
-        String expectedRecipient = "test@example.com";
-        String expectedBody = "This is a test email";
+    void callsAfterSaveActionAfterSaveInSubflow() throws Exception {
 
         ResultActions test = postWithoutData("testFlow", "subflowIterationStart/new");
         String url = test.andExpect(status().is3xxRedirection())
@@ -89,12 +66,6 @@ public class AfterSaveActionTest extends AbstractMockMvcTest {
         assertThat(url).isNotNull();
         assertThat(url.contains("next"));
 
-        verify(mailgunMessagesApi, times(1)).sendMessage(any(), any());
-
-        final Message builtMessage = captor.getValue();
-        AssertionsForClassTypes.assertThat(builtMessage).isNotNull();
-        AssertionsForClassTypes.assertThat(builtMessage.getSubject()).isEqualTo(expectedSubject);
-        AssertionsForClassTypes.assertThat(builtMessage.getTo().contains(expectedRecipient)).isEqualTo(true);
-        AssertionsForClassTypes.assertThat(builtMessage.getHtml()).isEqualTo(expectedBody);
+        verify(submissionRepository, times(1)).findById(any(UUID.class));
     }
 }
